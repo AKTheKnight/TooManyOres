a775fdfe0f5ccd28bbb669470543c45f11254fd8
AKTheKnight <AKTheKnight.minecraft@gmail.com>
2016-01-01 22:49:47 +0000

    Released 1.0.4 as Beta. Using to test ore disabling

--- src/java/com/aktheknight/toomanyores/TooManyOres.java
+++ src/java/com/aktheknight/toomanyores/TooManyOres.java
@@ -17,12 +17,12 @@ import cpw.mods.fml.common.event.FMLLoadCompleteEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.common.MinecraftForge;
 
-@Mod(modid="TooManyOres", name="Too Many Ores", version="1.7.10-1.0.3")
+@Mod(modid="TooManyOres", name="Too Many Ores", version="1.7.10-1.0.4")
 
 public class TooManyOres {
   public static final String MODID = "TooManyOres";
   public static final String MODNAME = "Too Many Ores";
-  public static final String VERSION = "1.7.10-1.0.3";
+  public static final String VERSION = "1.7.10-1.0.4";
   
   @Mod.Instance
   public static TooManyOres instance;
