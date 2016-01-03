987c0ed3e4f7ab335bf16a0b1068866ed45ac6cf
AKTheKnight <AKTheKnight.minecraft@gmail.com>
2016-01-03 14:56:26 +0000

    No issues found. Update to 1.0.5

--- src/java/com/aktheknight/toomanyores/TooManyOres.java
+++ src/java/com/aktheknight/toomanyores/TooManyOres.java
@@ -17,12 +17,12 @@ import cpw.mods.fml.common.event.FMLLoadCompleteEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.common.MinecraftForge;
 
-@Mod(modid="TooManyOres", name="Too Many Ores", version="1.7.10-1.0.4")
+@Mod(modid="TooManyOres", name="Too Many Ores", version="1.7.10-1.0.5")
 
 public class TooManyOres {
   public static final String MODID = "TooManyOres";
   public static final String MODNAME = "Too Many Ores";
-  public static final String VERSION = "1.7.10-1.0.4";
+  public static final String VERSION = "1.7.10-1.0.5";
   
   @Mod.Instance
   public static TooManyOres instance;
